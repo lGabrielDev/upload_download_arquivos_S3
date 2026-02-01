a3ac65021709abc10774f90fa82e2f6d84cb7a9a
lGabrielDev <thegabrielfreitasbf@yahoo.com.br>
2026-01-31 22:45:17 -0300

    removido aws credentials do properties

--- src/main/java/br/com/lgabrieldev/download_upload_S3/aws/S3Config.java
+++ src/main/java/br/com/lgabrieldev/download_upload_S3/aws/S3Config.java
@@ -13,26 +13,9 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-     @Profile({"local", "dockerizado"})
      @Bean
-     public S3Client generateS3Client(  @Value("${aws_access_key}") String accessKey, //o spring so vai procurar esses cara se o profile 'dev' tiver ativo
-                                                            @Value("${aws_secret_access_key}") String secretAcessKey,
-                                                            @Value("${aws_region}") String region){
+     public S3Client generateS3ClientProducao(){ //esse @Value vai procurar sempre no profile que tiver ativo
           return S3Client.builder()
-               .region(Region.of(region))
-               .credentialsProvider(
-                    StaticCredentialsProvider.create(
-                         AwsBasicCredentials.create(accessKey, secretAcessKey)
-                    )
-               )
-               .build();
-     }
-
-     @Profile("devaws") //esse bean so vai ser criado se esse profile tiver ativo
-     @Bean
-     public S3Client generateS3ClientProducao(@Value("${aws_region}") String region){ //esse @Value vai procurar sempre no profile que tiver ativo
-          return S3Client.builder()
-               .region(Region.of(region))
                .credentialsProvider(DefaultCredentialsProvider.builder().build()) // as credenciais vao ser passadas de forma automática pela AWS SDK, através da role dessa EC2
                //quando voce coloca uma role no seu EC2, dando permissao de acesso ao serviço S3, voce não precisa passar credentials...... Essa maquina ja vai ter o acesso.
                .build();
